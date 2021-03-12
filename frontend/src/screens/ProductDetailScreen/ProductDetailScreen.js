630ba6073b9ca8d6b54c9888f0a122343aaf6aa6
rohitgeddam <rohitgeddam2018@gmail.com>
2021-03-12 23:07:36 +0530

    modify layout of Production detail screen

--- frontend/src/screens/ProductDetailScreen/ProductDetailScreen.js
+++ frontend/src/screens/ProductDetailScreen/ProductDetailScreen.js
@@ -12,53 +12,57 @@ const ProductDetailScreen = ({match}) => {
     const productData = getFakeProduct()
     console.log(productData)
     return (
-        <div className="flex flex-col h-auto w-full">
-            {/* <div class="flex-none relative w-full md:w-3/6 h-3/6"> */}
-                <img src={productData.thumbnail_url} alt="" class=" inset-0 w-full md:w-2/3 lg:w-1/3 h-full object-scale-down" />
-            {/* </div> */}
-            <div className="flex-auto mt-4 px-4">
+        <div className="flex flex-col  h-auto w-full p-4">
 
-                <h1 class="flex-auto text-xl font-semibold">
-                    {productData.product_name}
-                </h1>
-                <div class="text-xl font-semibold text-gray-500">
-                    ${productData.price}
-                </div>
-                <div className="w-full mt-4 rounded-md">
-                    <Dropdown placeholder="--Select--" clearable options={options} selection className="w-full rounded-md"/>
-                </div>
+            <div className="flex flex-col md:flex-row mt-4">
 
-                <div>
-                    {/* Quantity */}
-                </div>
+                <img src={productData.thumbnail_url} alt="" class="flex-none inset-0 w-full md:w-2/4  h-full object-left-top object-scale-down md:object-cover" />
 
-                <div className="w-full mt-4">
-                    <Button secondary className="w-full" circular>
-                        ADD TO CART
-                    </Button>
-                </div>
+                <div className="flex-auto mt-4 px-4 w-full md:w-1/6 lg:w-1/6">
 
-                <div className="w-full mt-8">
+                    <h1 class="flex-auto text-xl font-semibold">
+                        {productData.product_name}
+                    </h1>
+                    <div class="text-xl font-semibold text-gray-500">
+                        ${productData.price}
+                    </div>
+                    <div className="w-full mt-4 rounded-md">
+                        <Dropdown placeholder="--Select--" clearable options={options} selection className="w-full rounded-md"/>
+                    </div>
+
+                
+
+                    <div className="w-full mt-4">
+                        <Button secondary className="w-full" circular>
+                            ADD TO CART
+                        </Button>
+                    </div>
+
+                    <div className="w-full mt-8">
                     <p className="font-semibold text-xl text-gray-500">Description</p>
                     <div className="font-light text-md">
                         {productData.description}
                     </div>
                 </div>
 
-                <div className="w-full mt-8">
-                    <p className="font-semibold text-xl text-gray-500">Images</p>
+                </div>
+
+            </div>
+
+            <div className="w-full mt-8 flex flex-col">
+                    <p className="font-semibold text-xl text-gray-500">More Images</p>
  
                     {
 
                     productData.images.map((image) => {
 
                         return (
-                            <img src={image} className="w-full md:w-1/3 my-4 rounded-md object-scale-down"/>
+                            <img src={image} className="w-full md:w-2/4 my-4 rounded-md object-scale-down md:object-cover"/>
                         )
                     })
                     }
                 </div>
-            </div>
+
         </div>
     );
 }
